80faf91427f620d2825f7caafcbdf4bb068ab95b
Stephen Baker <stephengbaker@yahoo.com>
2019-07-10 02:11:42 -0500

    Add test string template literal

--- app.js
+++ app.js
@@ -3,6 +3,239 @@
 //TODO: feature add: checkbox option to have Psalm chantable
 
 
+const testString = `Sundays and Seasons
+
+Welcome back, Stephen Baker   
+     
+ July 2019  
+Su	Mo	Tu	We	Th	Fr	Sa
+ 	1	2	3	4	5	6
+7	8	9	10	11	12	13
+14	15	16	17	18	19	20
+21	22	23	24	25	26	27
+28	29	30	31	 	 	 
+View larger calendar
+Looking at Sunday, July 14
+Lectionary 15, Year C
+5th Sunday after Pentecost
+ Icon2 Lectionary 15C (BW)
+First Reading
+Deuteronomy 30:9-14
+Psalm
+Psalm 25:1-10
+Second Reading
+Colossians 1:1-14
+Gospel
+Luke 10:25-37
+Semicontinuous First Reading
+Amos 7:7-17
+Semicontinuous Psalm
+Psalm 82
+ 
+Download this content
+Sunday, July 14, 2019
+Lectionary 15, Year C
+Introduction
+To love the Lord your God with all your heart, soul, strength, and mind is to reflect God’s mercy in responding to one’s neighbor. That mercy found its most profound expression in the “gospel that has come to you”—namely the life, death, and resurrection of Jesus Christ. That gospel mercy comes to us again today: at the font, at the altar, and from the pulpit. It is very near to you.
+Confession and Forgiveness
+All may make the sign of the cross, the sign marked at baptism, as the presiding minister begins.
+Blessed be the holy Trinity, ☩ one God,
+who forgives all our sin,
+whose mercy endures forever.
+Amen.
+Seeking reconciliation with God and neighbor,
+let us remember the gift of baptism and confess our sin.
+Silence is kept for reflection.
+
+God of mercy,
+we confess that we have sinned against you,
+against one another,
+and against the earth entrusted to our care.
+We are worried and distracted by many things,
+and we fail to love you above all else.
+We store up treasures for ourselves
+and turn away from our neighbors in need.
+Forgive us, that we may live in the freedom of your Son,
+Jesus Christ our Lord. Amen.
+When we were laid low by sin and guilt,
+God made us alive together with Christ,
+forgiving us ☩ all our trespasses
+by taking our sins to the cross.
+For freedom Christ has set us free:
+Rejoice in this good news!
+Amen.
+Prayer of the Day
+O Lord God, your mercy delights us, and the world longs for your loving care. Hear the cries of everyone in need, and turn our hearts to love our neighbors with the love of your Son, Jesus Christ, our Savior and Lord.
+Amen.
+Gospel Acclamation
+Alleluia. You shall love the Lord your God with | all your heart,* and your neighbor | as yourself. Alleluia. (Luke 10:27)
+
+Readings and Psalm
+Deuteronomy 30:9-14 
+The Lord will take delight in your fruitfulness
+Psalm 25:1-10 
+Show me your ways, O Lord, and teach me your paths. (Ps. 25:4)
+Colossians 1:1-14 
+The gospel is growing, bearing fruit in the whole world
+Luke 10:25-37 
+The parable of the merciful Samaritan
+Semicontinuous First Reading and Psalm
+Amos 7:7-17 
+A plumb line will judge the people
+Psalm 82 
+Arise, O God, and rule the earth. (Ps. 82:8)
+First Reading: Deuteronomy 30:9-14
+Moses calls the people who are about to enter the promised land to renew the covenant God made with their ancestors. Through this covenant God gives life and asks for obedience. God’s commandment is neither burdensome nor too far off, but dwells in the people’s own hearts.
+9The Lord your God will make you abundantly prosperous in all your undertakings, in the fruit of your body, in the fruit of your livestock, and in the fruit of your soil. For the Lord will again take delight in prospering you, just as he delighted in prospering your ancestors, 10when you obey the Lord your God by observing his commandments and decrees that are written in this book of the law, because you turn to the Lord your God with all your heart and with all your soul.
+  11Surely, this commandment that I am commanding you today is not too hard for you, nor is it too far away. 12It is not in heaven, that you should say, “Who will go up to heaven for us, and get it for us so that we may hear it and observe it?” 13Neither is it beyond the sea, that you should say, “Who will cross to the other side of the sea for us, and get it for us so that we may hear it and observe it?” 14No, the word is very near to you; it is in your mouth and in your heart for you to observe.
+Psalm: Psalm 25:1-10
+Show me your ways, O Lord, and teach me your paths. (Ps. 25:4)
+ 1To | you, O Lord,
+  I lift | up my soul.
+ 2My God, I put my trust in you; let me not be | put to shame,
+  nor let my enemies triumph | over me.
+ 3Let none who look to you be | put to shame;
+  rather let those be put to shame | who are treacherous.
+ 4Show me your | ways, O Lord,
+  and teach | me your paths. R
+ 5Lead me in your | truth and teach me,
+  for you are the God of my salvation; in you have I trusted all | the day long.
+ 6Remember, O Lord, your compas- | sion and love,
+  for they are from | everlasting.
+ 7Remember not the sins of my youth and | my transgressions;
+  remember me according to your steadfast love and for the sake of your good- | ness, O Lord.
+ 8You are gracious and up- | right, O Lord;
+  therefore you teach sinners | in your way.
+ 9You lead the low- | ly in justice
+  and teach the low- | ly your way.
+ 10All your paths, O Lord, are steadfast | love and faithfulness
+  to those who keep your covenant and your | testimonies. R
+Second Reading: Colossians 1:1-14
+The letter to the Colossians was written to warn its readers of various false teachings. The first part of the letter is an expression of thanks for the faith, hope, and love that mark this community, including a prayer for strength and courage from Paul.
+1Paul, an apostle of Christ Jesus by the will of God, and Timothy our brother,
+  2To the saints and faithful brothers and sisters in Christ in Colossae: 
+  Grace to you and peace from God our Father.
+
+  3In our prayers for you we always thank God, the Father of our Lord Jesus Christ, 4for we have heard of your faith in Christ Jesus and of the love that you have for all the saints, 5because of the hope laid up for you in heaven. You have heard of this hope before in the word of the truth, the gospel 6that has come to you. Just as it is bearing fruit and growing in the whole world, so it has been bearing fruit among yourselves from the day you heard it and truly comprehended the grace of God. 7This you learned from Epaphras, our beloved fellow servant. He is a faithful minister of Christ on your behalf, 8and he has made known to us your love in the Spirit.
+  9For this reason, since the day we heard it, we have not ceased praying for you and asking that you may be filled with the knowledge of God’s will in all spiritual wisdom and understanding, 10so that you may lead lives worthy of the Lord, fully pleasing to him, as you bear fruit in every good work and as you grow in the knowledge of God. 11May you be made strong with all the strength that comes from his glorious power, and may you be prepared to endure everything with patience, while joyfully 12giving thanks to the Father, who has enabled you to share in the inheritance of the saints in the light. 13He has rescued us from the power of darkness and transferred us into the kingdom of his beloved Son, 14in whom we have redemption, the forgiveness of sins.
+Gospel: Luke 10:25-37
+Jesus is challenged to explain what is involved in obeying the greatest commandment. Jesus tells a parable rich in surprises: those expected to show pity display hard hearts while the lowly give and receive unexpected and lavish mercy.
+25Just then a lawyer stood up to test Jesus. “Teacher,” he said, “what must I do to inherit eternal life?” 26He said to him, “What is written in the law? What do you read there?” 27He answered, “You shall love the Lord your God with all your heart, and with all your soul, and with all your strength, and with all your mind; and your neighbor as yourself.” 28And he said to him, “You have given the right answer; do this, and you will live.”
+  29But wanting to justify himself, he asked Jesus, “And who is my neighbor?” 30Jesus replied, “A man was going down from Jerusalem to Jericho, and fell into the hands of robbers, who stripped him, beat him, and went away, leaving him half dead. 31Now by chance a priest was going down that road; and when he saw him, he passed by on the other side. 32So likewise a Levite, when he came to the place and saw him, passed by on the other side. 33But a Samaritan while traveling came near him; and when he saw him, he was moved with pity. 34He went to him and bandaged his wounds, having poured oil and wine on them. Then he put him on his own animal, brought him to an inn, and took care of him. 35The next day he took out two denarii, gave them to the innkeeper, and said, ‘Take care of him; and when I come back, I will repay you whatever more you spend.’ 36Which of these three, do you think, was a neighbor to the man who fell into the hands of the robbers?” 37He said, “The one who showed him mercy.” Jesus said to him, “Go and do likewise.”
+Semicontinuous First Reading: Amos 7:7-17
+Amos, a shepherd from the southern village of Tekoa, is called by God to preach against Israel, the Northern Kingdom, in a time of economic prosperity. Today’s reading illustrates how Amos’s stinging criticism of Israel alienated him from those in authority, including King Jeroboam and the priest Amaziah.
+7This is what [the Lord God] showed me: the Lord was standing beside a wall built with a plumb line, with a plumb line in his hand. 8And the Lord said to me, “Amos, what do you see?” And I said, “A plumb line.” Then the Lord said, 
+ “See, I am setting a plumb line
+  in the midst of my people Israel;
+  I will never again pass them by;
+ 9the high places of Isaac shall be made desolate,
+  and the sanctuaries of Israel shall be laid waste,
+  and I will rise against the house of Jeroboam with the sword.”
+  10Then Amaziah, the priest of Bethel, sent to King Jeroboam of Israel, saying, “Amos has conspired against you in the very center of the house of Israel; the land is not able to bear all his words. 11For thus Amos has said, 
+ ‘Jeroboam shall die by the sword,
+  and Israel must go into exile
+  away from his land.’ ”
+12And Amaziah said to Amos, “O seer, go, flee away to the land of Judah, earn your bread there, and prophesy there; 13but never again prophesy at Bethel, for it is the king’s sanctuary, and it is a temple of the kingdom.”
+  14Then Amos answered Amaziah, “I am no prophet, nor a prophet’s son; but I am a herdsman, and a dresser of sycamore trees, 15and the Lord took me from following the flock, and the Lord said to me, ‘Go, prophesy to my people Israel.’
+ 16“Now therefore hear the word of the Lord.
+ You say, ‘Do not prophesy against Israel,
+  and do not preach against the house of Isaac.’
+ 17Therefore thus says the Lord:
+ ‘Your wife shall become a prostitute in the city,
+  and your sons and your daughters shall fall by the sword,
+  and your land shall be parceled out by line;
+ you yourself shall die in an unclean land,
+  and Israel shall surely go into exile away from its land.’ ”
+Semicontinuous Psalm: Psalm 82
+Arise, O God, and rule the earth. (Ps. 82:8)
+ 1God stands to charge the divine coun- | cil assembled,
+  giving judgment in the midst | of the gods:
+ 2“How long will you | judge unjustly,
+  and show favor | to the wicked?
+ 3Save the weak | and the orphan;
+  defend the hum- | ble and needy;
+ 4rescue the weak | and the poor;
+  deliver them from the power | of the wicked. R
+ 5They do not know, neither do they understand; they wander a- | bout in darkness;
+  all the foundations of the | earth are shaken.
+ 6Now I say to you, | ‘You are gods,
+  and all of you children of | the Most High;
+ 7nevertheless, you shall | die like mortals,
+  and fall like | any prince.’ ”
+ 8Arise, O God, and | rule the earth,
+  for you shall take all nations | for your own. R
+Prayers of Intercession
+The prayers are prepared locally for each occasion. The following examples may be adapted or used as appropriate.
+
+Joining our voices with God’s people around the world, let us offer our prayers for those in need.
+
+A brief silence.
+
+For the church, steadfast and faithful in its mission to proclaim redemption through Christ Jesus; for all ministers of the gospel who proclaim that the word is near, let us pray.
+Have mercy, O God.
+For areas affected by drought or storms; for livestock and fields; for ranchers and farmers; and for all stewards of the earth, that as God’s goodness is revealed in creation, we act with justice toward all creatures, let us pray.
+Have mercy, O God.
+For lawyers and advocates; for local, regional, and national governments; and for peace throughout the world, that God send gracious and upright leaders to govern with mercy and truth, let us pray.
+Have mercy, O God.
+For those who feel ashamed; for those who find it difficult to trust; for the bereaved and sick (especially), that God provide compassionate and loving caregivers to all who suffer, let us pray.
+Have mercy, O God.
+For the members of the body of Christ in this place; for those who do good works in our midst; for those who are visiting and those who are absent, that the Holy Spirit guide all the journeys of our lives, let us pray.
+Have mercy, O God.
+Here other intercessions may be offered.
+
+For our ancestors who have inspired us by their lives of faith, that thankful for their witness, we can confidently proclaim our salvation, let us pray.
+Have mercy, O God.
+Merciful God, you hear the prayers of your people even before they are spoken. We commend these and all our prayers to you, trusting in your abundant mercy; through Jesus Christ, our Savior and Lord.
+Amen.
+Offering Prayer
+Blessed are you, O God,
+for the greening earth given for all,
+for the talents we are given to share,
+and for this bread and wine.
+Transform us to be the body of Christ,
+that, feasting on this food and drink,
+our lives may reflect your generosity;
+through Christ our Lord.
+Amen.
+Invitation to Communion
+God invites you to this table of bounty.
+Come, the banquet is ready.
+Prayer after Communion
+O God, in this holy communion
+you have welcomed us into your presence,
+nourished us with words of mercy,
+and fed us at your table.
+Amid the cares of this life,
+strengthen us to love you with all our heart,
+serve our neighbors with a willing spirit,
+and honor the earth you have made;
+through Christ our Lord.
+Amen.
+Blessing
+Live your lives in Christ, rooted and built up in him,
+and abound in thanksgiving;
+and the blessing of the holy Trinity, ☩ one God,
+be upon you and remain with you forever.
+Amen.
+Dismissal
+Go in peace. Christ is sending you.
+Thanks be to God.
+
+
+Upcoming Commemorations
+Benedict of Nursia, Abbot of Monte Cassino, died around 540
+Thursday, July 11, 2019
+Benedict is known as the father of the Western monastic movement. Educated in Rome, he went to live as a hermit, and eventually gathered a community of monks around him. He wrote a rule for living in monasteries that is used by Benedictines and adapted by others.
+Nathan Söderblom, Bishop of Uppsala, died 1931
+Friday, July 12, 2019
+A winner of the Nobel Prize for peace for his efforts to unite peoples after the First World War, Söderblom also called attention to the revival of early church worship principles, and helped to lay the groundwork for the World Council of Churches.
+Bartolomé de Las Casas, missionary to the Indies, died 1566
+Wednesday, July 17, 2019
+A native of Spain, Las Casas first came to the Western hemisphere while serving in the military. Granted a large estate with indigenous slaves, he freed them after he was ordained a priest. He worked in the Caribbean and Central America to improve the lives of the native peoples.
+PLANNER LIBRARY MUSIC PREACHING VISUALS NRSV BIBLE HELP CONTACT US
+Sundays and Seasons v.20190703.1054
+Copyright © 2019 Augsburg Fortress. All rights reserved.`;
+
 
 var input = document.getElementById("input");
 var parseButton = document.getElementById("parse");
